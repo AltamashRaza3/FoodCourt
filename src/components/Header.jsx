5782dd095b8536f71a0090123c21b67a3fc0488f
Altamash Raza <altamashraza3.ar@gmail.com>
2025-08-09 13:21:49 +0530

    Adding Logout and Login button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
 
 
 const Header = () => {
+const [btnName,setBtnName] = useState("Login")
+
   return (
     <div>
       <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
@@ -14,14 +16,23 @@ const Header = () => {
           </div>
 
           {/* Navigation */}
-          <nav>
-            <ul className="flex gap-8 items-center text-lg text-gray-700 font-medium">
+          <nav className="w-full mt-4 md:mt-0 md:w-auto">
+            <ul className="flex gap-6 items-center text-lg text-gray-700 font-medium">
               <li className="hover:text-orange-500 cursor-pointer">Home</li>
               <li className="hover:text-orange-500 cursor-pointer">About Us</li>
               <li className="hover:text-orange-500 cursor-pointer">
                 Contact Us
               </li>
               <li className="hover:text-orange-500 cursor-pointer">Cart</li>
+              <button className="login px-4 py-1 border border-orange-500 rounded-md text-orange-500 hover:bg-orange-500 hover:text-white transition-colors"
+              onClick={()=>{
+                btnName ==="Login"
+                ? setBtnName("Logout")
+                : setBtnName("Login")
+              }}
+              >
+                {btnName}
+              </button>
             </ul>
           </nav>
         </div>
