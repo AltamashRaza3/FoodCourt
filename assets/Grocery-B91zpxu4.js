b35c99acdba444a2214dcf02d099b68d67da90de
Altamash Raza <altamashraza3.ar@gmail.com>
2025-09-08 00:42:11 +0530

    Updates

--- assets/Grocery-B91zpxu4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-D1TpCGy3.js";const e=()=>r.jsx("h1",{children:"Our Grocery store is comming very soon"});export{e as default};
---
+++ assets/Grocery-fiF9tJiL.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-L5C5KYQO.js";const e=()=>r.jsx("h1",{children:"Our Grocery store is comming very soon"});export{e as default};
