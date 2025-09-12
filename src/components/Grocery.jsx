6a3f875304a6d982d8893b9df6c792bc1dd9ae44
Altamash Raza <altamashraza3.ar@gmail.com>
2025-09-12 22:57:27 +0530

    update grocery store

--- src/components/Grocery.jsx
+++ src/components/Grocery.jsx
@@ -1,9 +1,11 @@
-const Grocery =() =>{
-  return(
-    <h1>
-      Our Grocery store is comming very soon 
-    </h1>
-  )
-}
+const Grocery = () => {
+  return (
+    <div className="flex justify-center items-center min-h-[40vh]">
+      <h1 className="text-2xl md:text-3xl text-center text-white p-4">
+        Our Grocery store is coming very soon
+      </h1>
+    </div>
+  );
+};
 
-export default Grocery
+export default Grocery;
