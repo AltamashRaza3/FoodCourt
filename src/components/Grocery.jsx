78c298fdd132716dae140eda4cf224ff2326f940
Altamash Raza <altamashraza3.ar@gmail.com>
2025-09-19 23:55:58 +0530

    Update grocery

--- src/components/Grocery.jsx
+++ src/components/Grocery.jsx
@@ -2,7 +2,7 @@ const Grocery = () => {
   return (
     <div className="flex justify-center items-center min-h-[40vh]">
       <h1 className="text-2xl md:text-3xl text-center text-white p-4">
-        Our Grocery store is coming very soon
+        Our Grocery store is coming very soon...
       </h1>
     </div>
   );
